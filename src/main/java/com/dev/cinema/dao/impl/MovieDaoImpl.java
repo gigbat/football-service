5ac86241c2a50073922c392f2ea582cf489c88c3
gigbat <b.kostevich@gmail.com>
2021-01-26 15:28:45 +0200

    Practice with hibernate. First lesson

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,41 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long id = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(id);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert movie entity", e);
+        }
+        return movie;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Movie> queryAllMovies = session.createQuery("from Movie", Movie.class);
+            return queryAllMovies.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error retrieving all movies", e);
+        }
+    }
+}
