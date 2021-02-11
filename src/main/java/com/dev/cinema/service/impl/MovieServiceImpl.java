7417dede12845dbdf6eea54bdc3028694fc0e23d
gigbat <b.kostevich@gmail.com>
2021-02-11 15:08:57 +0200

    Introduce commit

--- src/main/java/com/dev/cinema/service/impl/MovieServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/MovieServiceImpl.java
@@ -1,16 +1,20 @@
 package com.dev.cinema.service.impl;
 
 import com.dev.cinema.dao.MovieDao;
-import com.dev.cinema.lib.Inject;
-import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class MovieServiceImpl implements MovieService {
-    @Inject
-    private MovieDao movieDao;
+    private final MovieDao movieDao;
+
+    @Autowired
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
 
     @Override
     public Movie add(Movie movie) {
