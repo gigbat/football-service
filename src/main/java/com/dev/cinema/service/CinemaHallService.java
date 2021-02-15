0d4ee3df4bf871ab1077497c9d5bb7119d29fda6
gigbat <b.kostevich@gmail.com>
2021-02-15 11:41:27 +0200

    Fixed TV

--- src/main/java/com/dev/cinema/service/CinemaHallService.java
+++ src/main/java/com/dev/cinema/service/CinemaHallService.java
@@ -2,7 +2,6 @@ package com.dev.cinema.service;
 
 import com.dev.cinema.model.CinemaHall;
 import java.util.List;
-import java.util.Optional;
 
 public interface CinemaHallService {
     CinemaHall add(CinemaHall cinemaHall);
--- src/main/java/com/dev/cinema/service/MovieService.java
+++ src/main/java/com/dev/cinema/service/MovieService.java
@@ -2,7 +2,6 @@ package com.dev.cinema.service;
 
 import com.dev.cinema.model.Movie;
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieService {
     Movie add(Movie movie);
